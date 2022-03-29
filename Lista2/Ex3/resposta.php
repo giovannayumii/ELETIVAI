75ca8b1f82975ccc8d34e2a39378f58ad2252cbc
giovannayumii <100042126+giovannayumii@users.noreply.github.com>
2022-03-28 21:56:34 -0300

    Lista3

--- Lista2/Ex3/resposta.php
+++ Lista2/Ex3/resposta.php
@@ -1,16 +1,66 @@
-for ($i=1; $i<=10; $i++)
-      {
-        if ($i == $i)
-        {
-            $substituir[$i] = "-" ;
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-            foreach ($ordenado as $chave => $valor)
-            {
-                echo "<br>";
-                echo "Posição do Vetor: $chave e seu valor é $valor";
-                echo "<br>";
-            }
-            
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Lista 2 resposta 3</title>
+  </head>
+  <body class ="container">
+    <h1> Lista2 - Resposta3</h1>
+
+    <?php
+  
+      for ($i=1; $i<=10; $i++)
+      {
+        $ordenado[$i] = $_POST["valor$i"];
+      }
+      
+      echo "<br>";
+      echo "ORDENADO";
+      echo "<br>";
+      
+      function numOrdenado($ordenado)
+      {
+        sort($ordenado);
+        foreach ($ordenado as $chave => $valor)
+        {
+          echo "<br>";
+          echo "Posição do Vetor: $chave e seu valor é $valor";
+          echo "<br>";
         }
       }
-    
+      numOrdenado($ordenado);
+
+      for ($i=1; $i<=10; $i++)
+      {
+        $substituir[$i] = $_POST["valor$i"];
+      }
+        if ("valor$i == valor$i")
+        {
+          $valor = "-";
+          echo "<br>";
+          echo "Posição do Vetor: $chave e seu valor é $valor";
+          echo "<br>";
+        }
+        numOrdenado($ordenado);
+
+  
+    ?>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
--- Lista3/Ex3/resposta.php
+++ Lista3/Ex3/resposta.php
@@ -7,39 +7,32 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Lista 2 - exercicio 3</title>
+    <title>Lista 3 - ex 3 resposta</title>
   </head>
+  
   <body class ="container">
-    <h1> Lista2 - Exercicio3</h1>
+    <h1>Cálculos</h1>
 
-    <form action = "resposta.php" method = "POST">
-        <div class ="row">
-            <?php
-            for ($i=1; $i<=10; $i++)
-            {
-                ?>
-                <div class ="col">
-                    <label for ="valor<? $i ?>" class= "label-control">
-                        Informe o valor <?=$i?>
-                    </label>
-                    <input type = "number" name="valor<?=$i ?>" id="valor<?=$i ?>"
-                        class = "form-control"/>
-                </div>
-            <?php
-            }
-            ?>
-        </div>
+    <?php
+        $valor1 = $_POST['valor1'];
+        $valor2 = $_POST['valor2'];
 
-        <div class = "row mt-5">
-            <div class = "col">
-                <button class = "btn btn-primary"> Enviar </button>
-            </div>
-        </div>
+        echo "Valor 1 é: $valor1 e o valor 2 é : $valor2";
+        echo "<br/>";
 
-    </form>
+        $maior = " ";
 
-    
+        if ($valor1 > $maior)
+        {
+            $maior = $valor1;
+        }
+        if ($valor2 > $maior)
+        {
+            $maior = $valor2;
+        }
+        echo "O maior valor é $maior";
+
+    ?>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
