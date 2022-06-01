4ede46afda95abd9e48ee72776f2f116061035da
giovannayumii <100042126+giovannayumii@users.noreply.github.com>
2022-06-01 20:44:04 -0300

    COMMIT

--- ELETIVA/bootstrap.php
+++ ELETIVA/bootstrap.php
@@ -1,30 +1,47 @@
 <?php
-    require_once __DIR__.'/vendor/autoload.php';
 
-    //Aqui a gente recupera o que o usuario digitou e qual metodo HTTP ele utilizou.    
-    $method = $_SERVER['REQUEST_METHOD'];
-    $path = $_SERVER['PATH_INFO'];
+    require_once __DIR__ . "/vendor/autoload.php";
 
-    //Instanciar classe ROUTER
+    //AQUI A GENTE RECUPERA O QUE O USUÁRIO DIGITOU E QUAL MÉTODO HTTP ELE UTILIZOU
+
+    $method = $_SERVER["REQUEST_METHOD"];
+    $path = $_SERVER["PATH_INFO"];
+
+    //INSTANCIAR CLASSE ROUTER
+
     $router = new \Aluno\Eletiva\Router($method, $path);
 
-    //ADICIONAR AS ROTAS VALIDAS A BAIXO
+    //ADICIONAR AS ROTAS VÁLIDAS ABAIXO
 
-    $router->get('/ola-mundo', function()
-    { 
-        //Ver o return como um echo
+    $router->get("/ola-mundo", function(){
         return "Olá Mundo!";
     });
 
+<<<<<<< HEAD
     //ADICIONAR AS ROTAS VALIDAS A CIMA
+=======
+    $router->get('/cliente/novo','Aluno\Eletiva\Controller\ClientesController::abrirFormularioInserir');
+    $router->post('/cliente/inserir','Aluno\Eletiva\Controller\ClientesController::inserirCliente');
 
-    $result = $router ->handler();
+    $router->get('/produto/novo','Aluno\Eletiva\Controller\ProdutoController::abrirFormularioInserir');
+    $router->post('/produto/inserir','Aluno\Eletiva\Controller\ProdutoController::inserirProduto');
 
-    if(!$result)
-    {
+    $router->get('/exemplo','Aluno\Eletiva\Controller\ExercicioController::exibir');
+    $router->post("/exemplo-resultado", 'Aluno\Eletiva\Controller\ExercicioController::exibirResultado');
+
+    $router->get('/clientes','Aluno\Eletiva\Controller\ClientesController::abrirListaClientes');
+>>>>>>> b16b900f933307e8a6c298f6857bccf1a170fa4a
+
+    //ADICIONAR AS ROTAS VÁLIDAS ACIMA
+
+    $result = $router->handler();
+
+    if (!$result){
         http_response_code(404);
         echo "Página não encontrada";
         die();
-    }
+        }
+
     echo $result($router->getParams());
 
+?>
